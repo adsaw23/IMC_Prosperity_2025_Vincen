71393744f0c2cd792078cb61cdb7c01fcf9747c3
Disha Taneja <148480526+Disha1712@users.noreply.github.com>
2025-04-07 20:23:36 +0530

    Update Manual.cpp

--- Round 1/Manual.cpp
+++ Round 1/Manual.cpp
@@ -2,56 +2,48 @@
 using namespace std;
 
 const int N = 4;
+const int start = 3; 
+const int maxTrades = 5; 
 
 string names[N] = {"Snowballs", "Pizzas", "Silicon Nuggets", "SeaShells"};
 
-int main() {
-    double adj[N][N] = {
-        {1.00, 1.45, 0.52, 0.72},    // From Snowballs
-        {0.70, 1.00, 0.31, 0.48},    // From Pizzas
-        {1.95, 3.10, 1.00, 1.49},    // From Silicon Nuggets
-        {1.34, 1.98, 0.64, 1.00}     // From SeaShells
-    };
+double adj[N][N] = {
+    {1.00, 1.45, 0.52, 0.72},    // From Snowballs
+    {0.70, 1.00, 0.31, 0.48},    // From Pizzas
+    {1.95, 3.10, 1.00, 1.49},    // From Silicon Nuggets
+    {1.34, 1.98, 0.64, 1.00}     // From SeaShells
+};
 
-    int start = 3;  
-    vector<int> nodes;
-    for (int i = 0; i < N; ++i)
-        if (i != start) nodes.push_back(i);  
+double maxProduct = 0;
+vector<int> bestPath;
 
-    double maxProduct = 0;
-    vector<int> bestPath;
+void dfs(int current, vector<int>& path, double product, int trades) {
+    if (trades > maxTrades) return;
 
-    for (int mask = 1; mask < (1 << nodes.size()); ++mask) {
-        vector<int> subset;
-        for (int i = 0; i < nodes.size(); ++i) {
-            if (mask & (1 << i))
-                subset.push_back(nodes[i]);
+    if (trades == maxTrades && current == start) {
+        for (int i = 0; i < path.size(); ++i) {
+            cout << names[path[i]] << " -> ";
         }
+        cout << names[start] << " | Product = " << product << "\n";
 
-        sort(subset.begin(), subset.end());
-        do {
-            double product = 1.0;
-            int current = start;
-
-            cout << names[start] << " -> ";
-            for (int next : subset) {
-                product *= adj[current][next];
-                cout << names[next] << " -> ";
-                current = next;
-            }
-            product *= adj[current][start];
-            cout << names[start];
-            cout << " | Product = " << product << "\n";
-
-            if (product > maxProduct) {
-                maxProduct = product;
-                bestPath = {start};
-                bestPath.insert(bestPath.end(), subset.begin(), subset.end());
-                bestPath.push_back(start);
-            }
-
-        } while (next_permutation(subset.begin(), subset.end()));
+        if (product > maxProduct) {
+            maxProduct = product;
+            bestPath = path;
+            bestPath.push_back(start);
+        }
+        return;
     }
+
+    for (int next = 0; next < N; ++next) {
+        path.push_back(next);
+        dfs(next, path, product * adj[current][next], trades + 1);
+        path.pop_back();
+    }
+}
+
+int main() {
+    vector<int> path = {start};
+    dfs(start, path, 1.0, 0);
 
     cout << "\nBest cycle:\n";
     for (int x : bestPath)
@@ -59,6 +51,5 @@ int main() {
     cout << names[start] << "\n";
 
     cout << "Max product: " << maxProduct << endl;
-
     return 0;
 }
